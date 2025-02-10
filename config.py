4572f55bb34771a16c9f0ae26c05112748d725bf
pavel <pavelkocozlov@yandex.ru>
2025-02-10 16:18:13 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# База данных
+host = "192.168.81.54"
+user = "service_feedbackParser"
+password = "********"
+database = "crm-prod"
+
+# Временной промежуток между парсингом ссылок (минимум 6)
+timeout = 20
+
+# Абсолютный путь к Chrome драйверу
+executable_path = "/home/parser_yandex/chromedriver"
